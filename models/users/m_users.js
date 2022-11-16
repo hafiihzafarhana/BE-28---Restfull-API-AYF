9c2c524436b1ca0f98d2e8c80964f21d93ed0b76
hafiihzafarhana <hafiihza2002@gmail.com>
2022-11-16 13:45:26 +0700

    Hafi-tambah model user, buat login dan register

--- models/users/m_users.js
+++ models/users/m_users.js
@@ -1,41 +1,49 @@
 const { default: mongoose } = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-   // first_name:{
-   //    type:String,
-   //    required:true,
-   //    minlength:[4, 'Must be at least 4 character']
-   // },
-   // last_name:{
-   //    type:String,
-   //    required:true,
-   //    minlength:[4, 'Must be at least 4 character']
-   // },
-   // username:{
-   //    type:String,
-   //    required:true,
-   //    minlength:[8, 'Must be at least 8 character'],
-   //    unique:true
-   // },
-   // password:{
-   //    type:String,
-   //    required:true,
-   //    minlength:[8, 'Must be at least 8 character']
-   // },
-   // date_of_birth:{
-   //    type:Date,
-   //    required:true
-   // },
+   first_name:{
+      type:String,
+      required:true,
+      minlength:[4, 'Must be at least 4 character']
+   },
+   last_name:{
+      type:String,
+      required:true,
+      minlength:[4, 'Must be at least 4 character']
+   },
+   username:{
+      type:String,
+      required:true,
+      minlength:[8, 'Must be at least 8 character'],
+      unique:true
+   },
+   password:{
+      type:String,
+      required:true,
+      minlength:[8, 'Must be at least 8 character']
+   },
+   date_of_birth:{
+      type:Date,
+      required:true
+   },
    role:{
       type:mongoose.Schema.Types.ObjectId,
       ref:"Role",
       required:true
    },
-   // country:{
-   //    type:mongoose.Schema.Types.ObjectId,
-   //    ref:"Country",
-   //    required:true
-   // },
+   country:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Country",
+      required:true
+   },
+   email:{
+      type:String,
+      required:true
+   },
+   phone_number:{
+      type:String,
+      required:true
+   }
 
 });
 
